78a5df1276245d416cf31ab5762142a7d58ca558
SBMiso <shinsojeong@sinsojeong-ui-MacBook-Pro.local>
2016-09-25 19:33:01 +0900

    initial commit
    initial commit

---
+++ TV/Login/Login.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+  <title> 로그인 페이지 </title>
+  <meta charset="utf-8">
+</head>
+<body>
+  아이디 :
+</body>
+</html>
---
+++ TV/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TV 생중계</title>
+    <link href="./bootstrap.min.css" rel="stylesheet">
+    <link href="cover.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">TV ON-AIR</h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="#">Home</a></li>
+                  <li><a href="#">Features</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="./Login/Login.php">Login</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Do you want to see TV ON-AIR ?</h1>
+            <p class="lead">Click the button</p>
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-default">HomePage</a>
+            </p>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>문의사항 <span style="font-weight:bold">tls1458@naver.com</p>
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+  </body>
+</html>
