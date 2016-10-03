6f592ba79b37b5a0c1fb3f18dd3e6a92ec739b01
SBMiso <shinsojeong@sinsojeong-ui-MacBook-Pro.local>
2016-10-03 14:32:01 +0900

    commit

--- TV/Login/ie10-viewport-bug-workaround.js
+++ TV/Login/ie10-viewport-bug-workaround.js
@@ -1,15 +1,6 @@
-/*!
- * IE10 viewport hack for Surface/desktop Windows 8 bug
- * Copyright 2014 Twitter, Inc.
- * Licensed under the Creative Commons Attribution 3.0 Unported License. For
- * details, see http://creativecommons.org/licenses/by/3.0/.
- */
-
-// See the Getting Started docs for more information:
-// http://getbootstrap.com/getting-started/#support-ie10-width
-
 (function () {
   'use strict';
+
   if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
     var msViewportStyle = document.createElement('style')
     msViewportStyle.appendChild(
@@ -19,4 +10,5 @@
     )
     document.querySelector('head').appendChild(msViewportStyle)
   }
+
 })();
--- TV/index.php
+++ TV/index.php
@@ -7,10 +7,6 @@
     <title>TV 생중계</title>
     <link href="./bootstrap.min.css" rel="stylesheet">
     <link href="cover.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
@@ -23,13 +19,12 @@
 
           <div class="masthead clearfix">
             <div class="inner">
-              <h3 class="masthead-brand">TV ON-AIR</h3>
+              <h3 class="masthead-brand">SS TV</h3>
               <nav>
                 <ul class="nav masthead-nav">
                   <li class="active"><a href="#">Home</a></li>
-                  <li><a href="#">Features</a></li>
-                  <li><a href="#">Contact</a></li>
-                  <li><a href="./Login/Login.php">Login</a></li>
+                  <li><a href="#">TV-ONAIR</a></li>
+                  <li><a href="./Login/Login.php")>Login</a></li>
                 </ul>
               </nav>
             </div>
@@ -39,7 +34,7 @@
             <h1 class="cover-heading">Do you want to see TV ON-AIR ?</h1>
             <p class="lead">Click the button</p>
             <p class="lead">
-              <a href="#" class="btn btn-lg btn-default">HomePage</a>
+              <a href="#" class="btn btn-lg btn-default">TV ON-AIR</a>
             </p>
           </div>
 
@@ -55,10 +50,5 @@
       </div>
 
     </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-
   </body>
 </html>
